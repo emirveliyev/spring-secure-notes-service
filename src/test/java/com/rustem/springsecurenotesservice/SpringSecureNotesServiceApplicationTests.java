105f9c604cc5ceff54476644960b6e82f1eabd21
emirveliyev <rustememirveliyev@gmail.com>
2025-09-28 21:26:38 +0500

    test: добавил базовые тесты для сервиса

---
+++ src/test/java/com/rustem/springsecurenotesservice/SpringSecureNotesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rustem.springsecurenotesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecureNotesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
